bcdd9f36463de7ee083b3d898eb6a0c5e93e8834
thiagocontelli <thiagocontellid@gmail.com>
2023-09-19 12:34:55 -0300

    Update app theme

--- app/src/main/java/com/thiagocontelli/watertracker/ui/theme/Theme.kt
+++ app/src/main/java/com/thiagocontelli/watertracker/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue40,
+    secondary = Blue20,
+    tertiary = Blue30
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue40,
+    secondary = Blue20,
+    tertiary = Blue30
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,7 +41,7 @@ private val LightColorScheme = lightColorScheme(
 fun WaterTrackerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
